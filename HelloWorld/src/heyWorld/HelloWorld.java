68948ce1031ec2ca8b2b7903bb909f42086b905a
Nick Persson <nickp1995@hotmail.com>
2018-10-11 14:00:14 +0200

    Pushing Hello World

---
+++ HelloWorld/src/heyWorld/HelloWorld.java
@@ -0,0 +1,11 @@
+package heyWorld;
+
+public class HelloWorld {
+
+	public static void main(String[] Args) {
+
+		System.out.println("Hello World");
+		System.out.println("I have no mouth and I must scream");
+		System.out.println("gaaaaahhhhhhhhh");
+	}
+}
