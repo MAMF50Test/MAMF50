40641a4640e5c62fdbd3a7bd3d4d323af7e3584f
Niklas Bruce <niklas.bruce94@gmail.com>
2018-10-16 15:44:26 +0200

    Added print Haj

--- HelloWorld/src/heyWorld/HelloWorld.java
+++ HelloWorld/src/heyWorld/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] Args) {
 
+		System.out.println("Haj");
 		System.out.println("Hello World");
 		System.out.println("I have no mouth and I must scream");
 		System.out.println("gaaaaahhhhhhhhh");
