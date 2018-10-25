21e8aefb4e22725db4527a72c87e80b9841aecc4
Niklas Bruce <niklas.bruce94@gmail.com>
2018-10-25 14:53:08 +0200

    Revert "added print"
    This reverts commit 9c9d7af5412d9e376aa8f9228186dbb37dc65c15.

--- HelloWorld/src/heyWorld/HelloWorld.java
+++ HelloWorld/src/heyWorld/HelloWorld.java
@@ -7,7 +7,6 @@ public class HelloWorld {
 		System.out.println("Hello World");
 		System.out.println("I have no mouth and I must scream");
 		System.out.println("gaaaaahhhhhhhhh");
-		System.out.println("lool
-");
+		System.out.println("haj");
 	}
 }
